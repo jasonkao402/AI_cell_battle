9432f91fa54d853556d4448cd2a3864ac9d5296c
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-12-27 00:27:26 +0800

    basic

--- Assets/ai_ctrl.cs
+++ Assets/ai_ctrl.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ai_ctrl : MonoBehaviour
+[System.Serializable]
+public class geneData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float maxhp, curhp, consume;
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class ai_ctrl : MonoBehaviour{
+    public geneData d = new geneData();
+    void Start(){
+        d.maxhp = d.curhp = Random.Range(50, 100);
+        d.consume = Random.Range(0.01f, 0.075f);
+    }
+    private void FixedUpdate() {
+        if(d.curhp > 0)
+        {
+            transform.position += d.consume * (Vector3)Random.insideUnitCircle;
+            d.curhp -= d.consume;
+        }
+        else
+        {
+            Destroy(gameObject, 1);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        d.curhp += other.transform.localScale.x * 50f;
     }
 }
---
+++ Assets/foodSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodSpawn : MonoBehaviour
+{
+    public GameObject food;
+    public float maxcd, food_range;
+    float curcd;
+    // Update is called once per frame
+    void Update()
+    {
+        if(curcd < 0)
+        {
+            curcd = maxcd;
+            for(int i = 0; i<8; i++)
+            {
+                Instantiate(food, food_range*Random.insideUnitCircle, Quaternion.identity);
+            }
+        }
+        curcd -= Time.deltaTime;
+    }
+}
